3127b911b352f60d1430717f1c99ba8adde67d7f
Tamou <ahamada.tamou@gmail.com>
2017-10-26 16:55:54 +0200

    26/10/2017

--- ajoutClient.php
+++ ajoutClient.php
@@ -4,7 +4,19 @@ include "Autoload.php";
 include "funct/listCategorie.php";
 
 if(isset($_POST["inscriptionSubmit"])){ 
-    
+    $mClient= new ManagerClient();
+
+    //Creation du token
+   /*	$p = new OAuthProvider();
+   	$t = $p->generateToken(50);
+	$_POST["token"]=$t;*/
+   	$_POST["valide"]=false;
+   
+
+   	$oClient=new Client($_POST);
+
+
+   	var_dump($oClient);
 }
 
 $loaderfile = new Twig_Loader_Filesystem('view/');
